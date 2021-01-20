5a80ace6fb82e5a76602314d2e30dd8cee591faf
Sherry Li <sherryhli@users.noreply.github.com>
2021-01-20 01:23:16 -0500

    Add comments and tweaks

--- backend/graphql.js
+++ backend/graphql.js
@@ -1,28 +1,49 @@
+/**
+ * the GraphQL schema defines the data graph and the API (i.e. what we can do with the data graph)
+ * it contains type definitions and resolvers that define how data values are computed
+ *
+ * there are 2 special type definitions: Query and Mutation. They are the entrypoints into our API
+ * (there is actually a third special type called Subscription but we will not be working with it)
+ */
+
 import { makeExecutableSchema, gql } from "apollo-server-express";
 import { merge } from "lodash";
 
-import restaurantSchema from "./schemas/restaurantSchema";
+import restaurantType from "./types/restaurantType";
 import restaurantResolvers from "./resolvers/restaurantResolvers";
 
-/* base query schema, other queries extend this */
+/**
+ * base Query type definition, other queries extend this
+ * Query is used for read operations
+ *
+ * the gql tag is used to signal GraphQL syntax highlighting in IDEs and editors (the actual type definition is a string literal)
+ * VS Code extension: https://marketplace.visualstudio.com/items?itemName=GraphQL.vscode-graphql
+ */
 const query = gql`
   type Query {
     _empty: String
   }
 `;
 
-/* base mutation schema, other mutations extend this */
+/**
+ * base Mutation type definition, other mutations extend this
+ * Mutation is used for write operations
+ */
 const mutation = gql`
   type Mutation {
     _empty: String
   }
 `;
 
+/**
+ * combine the typeDefs and resolvers
+ * click into restaurantType and restaurantResolvers to learn more about typeDefs and resolvers
+ */
 const executableSchema = makeExecutableSchema({
     typeDefs: [
       query,
       mutation,
-      restaurantSchema
+      restaurantType
     ],
     resolvers: merge(
         restaurantResolvers
--- backend/resolvers/restaurantResolvers.js
+++ backend/resolvers/restaurantResolvers.js
@@ -1,6 +1,42 @@
 import { GraphQLError } from "graphql";
 import restaurantService from "../services/restaurantService";
 
+/**
+ * from the Apollo docs: a resolver is a function that's responsible for populating the data for a single field in your schema
+ *
+ * for each of our queries and mutations, we must define how the fields in their results are computed
+ * suppose we are executing this query (finding the restaurant with id = 1 and returning all of its fields):
+ *
+ * {
+ *   restaurant(id: 1) {
+ *     id
+ *     name
+ *     address
+ *     type
+ *     budget
+ *     description
+ *     rating
+ *   }
+ * }
+ *
+ * we expect this response:
+ *
+ * "restaurant": {
+ *   "id": "1",
+ *   "name": "Campus Pizza",
+ *   "address": "160 University Ave W #2, Waterloo, ON N2L 3E9",
+ *   "type": "Pizzeria",
+ *   "budget": "LOW",
+ *   "description": "giant slices, affordable prices",
+ *   "rating": 2
+ * }
+ *
+ * the resolver for the restaurant Query (lines 42-44) fetches the necessary data through restaurantService and serializes the requested fields for the response
+ * (the serialization is automatically handled by Apollo)
+ * note the arguments for the resolver function:
+ * - the first, _parent, is currently unused, but it contains the previously resolved field
+ * - the second consists of arguments to the query / mutation (using destructured syntax in this case)
+ */
 const restaurantResolvers = {
     Query: {
       restaurant: async (_parent, { id }) => {
