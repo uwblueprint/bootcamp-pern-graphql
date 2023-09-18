975902f45a357726a2d3ae51ac84448d987768cc
Vaaranan Yogalingam <69359660+vaaranan-y@users.noreply.github.com>
2023-09-17 21:12:39 -0400

    Create sign in UI (#1)
    * Added sign-in component with basic UI
    
    * Added submit state
    
    * Changed display based on login status
    
    * Saved sign in input (for later use)

---
+++ frontend/src/components/SignIn.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+import "./SignIn.scss";
+
+const SignIn = ({ isSubmitted, setIsSubmitted }) => {
+  const extraSecureCheckFunction = (event) => {
+    var { uname, pass } = document.forms[0];
+    event.preventDefault();
+    setIsSubmitted(true);
+  };
+
+  return (
+    <div className="form">
+      <form onSubmit={extraSecureCheckFunction}>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          {/* {renderErrorMessage("uname")} */}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {/* {renderErrorMessage("pass")} */}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
