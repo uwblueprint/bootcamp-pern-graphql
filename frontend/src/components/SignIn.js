d515c550ce1bd6f3c2afd5cb747986e6c1c82532
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2023-09-17 20:32:44 -0400

    Added sign-in component with basic UI

---
+++ frontend/src/components/SignIn.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./SignIn.scss";
+
+const SignIn = ({}) => {
+  return (
+    <div className="form">
+      <form>
+        <div className="input-container">
+          <label>Username </label>
+          <input type="text" name="uname" required />
+          {/* {renderErrorMessage("uname")} */}
+        </div>
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" required />
+          {/* {renderErrorMessage("pass")} */}
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
