133520920b4341136e44c50e6a85e14ce533e18d
Nandini Mehrotra <90271211+NandiniMeh@users.noreply.github.com>
2023-09-17 21:53:50 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,52 @@
-// Define global imports here
-import React from 'react';
+// Global imports
+import React, { useState } from 'react';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-// Define relative imports here
+// Relative imports
 import Title from './components/Title';
 import DisplayContainer from './components/DisplayContainer';
+import AddRestaurant from './components/AddRestaurant';
 
-/**
- * This is the root component of your React App.
- * All classes and components will be imported into the project when included in this class.
- */
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  cache: new InMemoryCache(),
+});
+
 const App = () => {
+  const [showForm, setShowForm] = useState(false);  // State to manage showing the form
+
+  const buttonStyle = {
+    display: 'block',
+    margin: '40px auto',
+    padding: '15px 30px',
+    borderRadius: '5px',
+    backgroundColor: '#007BFF',
+    color: 'white',
+    fontSize: '1.2rem',
+    border: 'none',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s',
+    textDecoration: 'none'
+  };
+
   return (
-    <div className="App">
-      <Title teamName="Subway"></Title>
-      <DisplayContainer />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Title teamName="Subway" />
+
+        {/* Ternary condition to display form or main content */}
+        {showForm ? (
+          <AddRestaurant onFormSubmit={() => setShowForm(false)} />
+        ) : (
+          <>
+            <DisplayContainer />
+            <button style={buttonStyle} onClick={() => setShowForm(true)} onMouseOver={e => e.target.style.backgroundColor = '#0056b3'} onMouseOut={e => e.target.style.backgroundColor = '#007BFF'}>
+              Add New Restaurant
+            </button>
+          </>
+        )}
+      </div>
+    </ApolloProvider>
   );
 }
 
